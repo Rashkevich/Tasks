f0c98c5f1e154bd3dd7f674e45fd55f19542a7ab
Rashkevich Andrei <iland@mail.ru>
2019-06-07 08:20:22 +0300

    task1 array of arrays

--- Tasks/src/by/tasks/algorithmization/array/of/array/Task1.java
+++ Tasks/src/by/tasks/algorithmization/array/of/array/Task1.java
@@ -7,7 +7,17 @@ package by.tasks.algorithmization.array.of.array;
 public class Task1 {
 
 	public static void main(String[] args) {
-		
+		int matrix[][]= {{5,2,3},{6,8,9},{12,4,9},{3,0,8},{13,10,14}};
+		for (int i=0;i<matrix.length;i++) {
+			if ((i==0)||(i%2==0)){
+				if (matrix[i][0]>matrix[i][matrix[i].length-1]) {
+					for (int j=0;j<matrix[i].length;j++) {
+						System.out.print(matrix[i][j]+" ");
+					}
+					System.out.println();
+				}
+			}
+		}
 
 	}
 
