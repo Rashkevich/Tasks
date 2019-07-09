03a7f92b94b40cb0ce58bbd9da28138c0ee096b7
Rashkevich Andrei <iland@mail.ru>
2019-07-09 14:22:50 +0300

    Threads port task

---
+++ Tasks/src/examples/thread/ship/port/Container.java
@@ -0,0 +1,14 @@
+package examples.thread.ship.port;
+
+public class Container {
+private String id;
+
+public Container(String id) {
+	this.id = id;
+}
+
+public String getId() {
+	return id;
+}
+
+}
